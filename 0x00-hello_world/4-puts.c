7490e761ee947ad2d11f23b6376fb171ccbe9742
Yemi-21 <yemsrachbeza@gmail.com>
2022-06-30 16:29:30 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,43 @@
+/**
+
+ * main - print hard coded string using puts
+ *
+ *  * ----
+ *
+ *   * no parameters
+ *
+ *    * Return: Zero value
+ *
+ *
+ *       */
+
+
+
 #include <stdio.h>
+
+#include <string.h>
+
+
+
+int main(void)
+
+{
+
+	char vaca[52];
+
+
+
+	strcpy(vaca, "\"Programming is like building a multilingual puzzle");
+
+
+
+	puts(vaca);
+
+
+
+	return (0);
+
+}
 
 
 
