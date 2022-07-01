611623ab54195094eda37377a79e447dbaf0febf
Yemi-21 <yemsrachbeza@gmail.com>
2022-07-01 04:17:58 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,47 +1,12 @@
 #include <stdio.h>
-
 /**
+ * main - Entry point
  *
- *  * main - prints to string
- *
- *   *
- *
- *    * Description: Prints "and that piece of art is useful..." without puts
- *
- *     *
- *
- *      * Return: 1
- *
- *       */
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
 
 {
-
-		char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-			long l = 59;
-
-				long fd = 1;
-
-					long syscall = 1;
-
-						long ret = 0;
-
-
-
-							__asm__ ("syscall"
-
-												: "=a" (ret)
-
-															: "a" (syscall),
-
-																		"D" (fd),
-
-																					"S" (s),
-
-																								"d" (l));
-
-								return (1);
-
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+return (1);
 }
