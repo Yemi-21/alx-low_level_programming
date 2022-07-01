ab579f09784b0458c4d160e1ef10fc360645b5af
Yemi-21 <yemsrachbeza@gmail.com>
2022-07-01 04:03:06 -0700

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,8 @@
 * Description: use printf
 * Return: 0
 */
-int main(void) 
-{	
-printf("%s", "with proper grammar, but the outcome is a piece of art,\n");	
+int main(void)
+{
+printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
