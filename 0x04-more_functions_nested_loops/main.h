c48de7277045c8c0ae4be9bd348e6d2be1f21b4f
Yemi-21 <yemsrachbeza@gmail.com>
2022-07-07 03:15:36 -0700

    Main

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -11,5 +11,7 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+int isPrime(long int n);
+long int biggestFactor(long int a);
 void print_number(int n);
 #endif /* MAIN_H */
