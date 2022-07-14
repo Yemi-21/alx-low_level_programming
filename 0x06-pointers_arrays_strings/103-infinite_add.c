9e57dfa1e368a591d4d64018a7bbc97161d72674
Yemi-21 <yemsrachbeza@gmail.com>
2022-07-14 06:35:19 -0700

    mmm

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,6 +12,11 @@ i++;
 return (i);
 }
 
+/**
+ * rev_string - reverses a string
+ * @s: the string to reverse
+ * Return: void
+ */
 char *rev_string(char *s)
 {
 int l = _strlen(s), i = 0;
@@ -24,7 +29,14 @@ s[i] = t;
 }
 return (s);
 }
-
+/**
+ * infinite_add - adds arbitrarily long string of digits
+ * @n1: the first digit string
+ * @n2: the second digit string
+ * @r: the result buffer
+ * @size_r: the size of result buffer
+ * Return: char pointer to buffer
+ */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 int l1 = _strlen(n1), l2 = _strlen(n2), i = 0, a, b, c = 0;
