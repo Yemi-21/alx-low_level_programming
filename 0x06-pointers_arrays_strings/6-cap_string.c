ef647068a97d262c86c3bcd45318a63db53e03af
Yemi-21 <yemsrachbeza@gmail.com>
2022-07-14 06:00:24 -0700

    Straing Cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
  * cap_string - capitalizes most of the words in a string.
- * @s: analized string.
- *
+ * @s: analized strin
  * Return: String with all words capitalized.
  */
 char *cap_string(char *s)
