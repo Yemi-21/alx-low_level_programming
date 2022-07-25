0c6bf615798eeba4a21256578400f526a80926fb
Yemi-21 <yemsrachbeza@gmail.com>
2022-07-25 04:39:15 -0700

    Malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,36 +1,36 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 /**
- * alloc_grid - nested loop to make grid
- * @width: width input
- * @height: height input
- * Return: pointer to 2 dim. array
+ * alloc_grid - Initializes with 0 a matrix
+ * @width: number of columns.
+ * @height: number of rows.
+ * Return: pointer to the matrix.
  */
 int **alloc_grid(int width, int height)
 {
-int **iarray;
-int i, n;
+int i, j;
+int **matrix;
 if (width <= 0 || height <= 0)
 return (NULL);
-iarray = malloc(sizeof(int) * height);
-if (iarray == NULL)
+matrix = (int **) malloc(height *sizeof(int *));
+if (matrix == NULL)
 {
-free(iarray);
+free(matrix);
 return (NULL);
 }
 for (i = 0; i < height; i++)
 {
-iarray[i] = malloc(sizeof(int) * width);
-if (iarray == NULL)
+*(matrix + i) = (int *) malloc(width *sizeof(int));
+if (*(matrix + i) == NULL)
 {
-free(iarray);
-return (NULL);
-}
-for (n = 0; n < width; n++)
-{
-iarray[i][n] = 0;
+for (j = 0; j < i; j++)
+free(*(matrix + j));
+free(matrix);
 }
 }
-return (iarray);
+for (i = 0; i < height; i++)
+for (j = 0; j < width; j++)
+*(*(matrix + i) + j) = 0;
+return (matrix);
 }
