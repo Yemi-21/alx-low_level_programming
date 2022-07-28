814ac8701ab969bb66d24c90172ac77d394cceaf
Yemi-21 <yemsrachbeza@gmail.com>
2022-07-28 05:12:07 -0700

    Malloc

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,7 +14,7 @@ int i, length;
 if (min > max)
 return (NULL);
 length = max - min + 1;
-newArray = malloc(length * sizeof(int));
+newArray = malloc(length *sizeof(int));
 if (newArray == NULL)
 return (NULL);
 for (i = 0; i < length; i++)
