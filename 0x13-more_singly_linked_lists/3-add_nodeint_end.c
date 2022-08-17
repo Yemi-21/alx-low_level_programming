81d6329ce1a145b6dba6f36cc4e1fdab979d0b0f
Yemi-21 <yemsrachbeza@gmail.com>
2022-08-17 01:38:16 -0700

    Singly linked lists

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -5,14 +5,24 @@
  * @head: data type pointer of the head/next node
  * Return: elements of the str new_node
  */
-listint_t *add_nodeint(listint_t **head, const int n)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-listint_t *new_node;
+listint_t *new_node, *tmp_node;
 new_node = malloc(sizeof(listint_t));
 if (new_node == NULL)
 return (NULL);
 new_node->n = n;
-new_node->next = *head;
+new_node->next = NULL;
+if (*head == NULL)
+{
 *head = new_node;
+}
+else
+{
+tmp_node = *head;
+while (tmp_node->next != NULL)
+tmp_node = tmp_node->next;
+tmp_node->next = new_node;
+}
 return (new_node);
 }
